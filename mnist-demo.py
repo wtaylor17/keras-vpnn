4a113cb9de5eaf4d915816ebef8d56ea3e7c894f
wtaylor17 <wtaylor@upei.ca>
2020-05-24 10:30:52 -0300

    modified the mnist demo

--- mnist-demo.py
+++ mnist-demo.py
@@ -1,9 +1,12 @@
 from vpnn import vpnn
-from vpnn.utils import mnist_generator
-from keras.optimizers import SGD
+import tensorflow as tf
+import keras
 
-generation_fn = mnist_generator()
-train_generator = map(lambda data: (data[0].reshape(-1,28*28), data[1]), generation_fn())
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+y_train = keras.utils.to_categorical(y_train)
+y_test = keras.utils.to_categorical(y_test)
+x_train = x_train.reshape(-1, 28*28) / 255
+x_test = x_test.reshape(-1, 28*28) / 255
 
 model = vpnn(28*28,
              n_rotations=3,
@@ -11,22 +14,21 @@ model = vpnn(28*28,
              out_ac='softmax',
              activation='cheby',
              cheby_M=2,
-             out_dim=10,
-             diagonal_M=0.01)
+             out_dim=10)
 model.compile(loss='categorical_crossentropy',
               metrics=['acc'],
-              optimizer=SGD(momentum=0.9))
+              optimizer='adam')
 model.summary()
 
-history = model.fit_generator(train_generator,
-                                epochs=10,
-                                steps_per_epoch=100).history
+history = model.fit(x_train, y_train,
+                    epochs=10, validation_data=[x_test, y_test],
+                    batch_size=256).history
 
 import matplotlib.pyplot as plt
 
 _, axs = plt.subplots(1, 2)
-axs[0].plot(history['loss'], label='loss')
+axs[0].plot(history['val_loss'], 'g', label='val loss')
 axs[0].legend()
-axs[1].plot(history['acc'], label='acc')
+axs[1].plot(history['val_acc'], 'k', label='val acc')
 axs[1].legend()
 plt.show()
